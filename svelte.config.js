8d552375bc7959a7afa1703c0ba957d63f15777c
Alexander Ameye <alexanderameye@gmail.com>
2026-03-07 15:38:19 +0000

    init

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from "@sveltejs/adapter-cloudflare";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      config: undefined,
+      platformProxy: {
+        configPath: undefined,
+        environment: undefined,
+        persist: undefined,
+      },
+      fallback: "plaintext",
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+    }),
+  },
+  compilerOptions: {
+    runes: true,
+  },
+};
+
+export default config;
